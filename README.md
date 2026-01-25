34e12defc65397a5b96bf5a80f77f9e81f16487e
textic <danielbienchido@gmail.com>
2026-01-25 10:29:56 -0300

    feat: Implement Dockerization and initial Spring Security configuration with JWT and CORS.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ### Docker ###
 docker run -d --name oracle-db -p 1521:1521 -e ORACLE_PASSWORD=secret -e APP_USER=transporte_user -e APP_PASSWORD=transporte_pass -e APP_USER_PASSWORD=transporte_pass gvenzl/oracle-xe
 
+
 ### ENV ###
 AZURE_TENANT_NAME
 AZURE_POLICY_NAME
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,17 +1,16 @@
 package com.example.demo.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +33,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://3.226.49.209"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
